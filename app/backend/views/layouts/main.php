dc55aa301fcf57db19dce5fd3c9cfd1af375d99a
Waqqas Jabbar <waqqas.jabbar@roadez.com>
2016-02-09 11:27:09 +0500

    Generated incident CRUD

--- app/backend/views/layouts/main.php
+++ app/backend/views/layouts/main.php
@@ -36,6 +36,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Incident', 'url' => ['/incident/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
