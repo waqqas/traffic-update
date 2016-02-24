b75375f1fdc3efe8181c789608c6f4f2175aee60
Waqqas Jabbar <waqqas.jabbar@roadez.com>
2016-02-24 23:50:17 +0500

    Added functionality to report incidents via SMS

--- app/console/controllers/SmsController.php
+++ app/console/controllers/SmsController.php
@@ -17,7 +17,8 @@ class SmsController extends Controller
 {
 
 
-    private function loadSettings($msisdn){
+    private function loadSettings($msisdn)
+    {
 
         // set user's preferred language as application language
 
@@ -31,10 +32,9 @@ class SmsController extends Controller
             'active' => 1,
         ]);
 
-        if( $setting == null){
+        if ($setting == null) {
             $language = Yii::$app->sourceLanguage;
-        }
-        else{
+        } else {
             $language = $setting->value;
         }
 
@@ -48,7 +48,7 @@ class SmsController extends Controller
             $mo = Smsmo::findOne(['id' => $id]);
             if ($mo) {
 //                Yii::info(print_r($mo->text,true));
-                $regex = "/" . Yii::$app->params['smsKeyword'] . "\\s*([route|lang|sub|now|unsub]*)(.*)/i";
+                $regex = "/" . Yii::$app->params['smsKeyword'] . "\\s*([route|lang|sub|now|unsub|report]*)(.*)/i";
 
 //                Yii::info('regex: '. $regex);
 
@@ -69,7 +69,7 @@ class SmsController extends Controller
 
                     Yii::info('command: ' . $command);
 
-                    if (in_array($command, ['sub', 'lang', 'route', 'now', 'unsub'])) {
+                    if (in_array($command, ['sub', 'lang', 'route', 'now', 'unsub', 'report'])) {
                         $runCommand = implode(' ', [
                             'sms/' . $command,
                             $mo->msisdn,
@@ -142,6 +142,9 @@ class SmsController extends Controller
                 $commandParams[0] = '08:00';
             } else {
                 if (is_numeric($commandParams[0])) $commandParams[0] .= 'am';
+//                if( preg_match( '/[0-9]+:[0-9]+/i', $commandParams[0])){
+//                    $commandParams[0] .= 'am';
+//                }
                 // convert user given time to 24-hour format
                 $commandParams[0] = date('G:i', strtotime($commandParams[0]));
             }
@@ -150,17 +153,21 @@ class SmsController extends Controller
                 $commandParams[1] = '17:00';
             } else {
                 if (is_numeric($commandParams[1])) $commandParams[1] .= 'pm';
+//                if( preg_match( '/[0-9]+:[0-9]+/i', $commandParams[0])){
+//                    $commandParams[0] .= 'pm';
+//                }
+
                 // convert user given time to 24-hour format
                 $commandParams[1] = date('G:i', strtotime($commandParams[1]));
             }
 
-            $command = implode( ' ', [
+            $command = implode(' ', [
                 'sms/now',
                 $msisdn,
                 "islamabad",        // TODO: get user's preferred location
             ]);
 
-            Yii::info( "SMS sending times: " . $commandParams[0] . " and " . $commandParams[1]);
+            Yii::info("SMS sending times: " . $commandParams[0] . " and " . $commandParams[1]);
 
             /** @var \common\components\Schedule $schedule */
             $schedule = Yii::$app->schedule;
@@ -175,6 +182,13 @@ class SmsController extends Controller
             $sms = Yii::t('sms', 'You will receive SMS daily at {amTime} and {pmTime}', [
                 'amTime' => $commandParams[0],
                 'pmTime' => $commandParams[1]
+            ]);
+
+            $sms .= "\n";
+
+            $sms .= Yii::t('sms', 'To unsubscribe, send {message} at {shortCode}', [
+                'message' => Yii::$app->params['smsKeyword'] . " UNSUB",
+                'shortCode' => Yii::$app->params['smsShortCode'],
             ]);
 
             SmsSender::queueSend($msisdn, $sms);
@@ -310,7 +324,6 @@ class SmsController extends Controller
 
                 $fromAddresses = Yii::$app->googleMaps->geocode($from);
 
-                /** @var \Geocoder\1Model\AddressCollection $toAddresses */
                 $toAddresses = Yii::$app->googleMaps->geocode($to);
 
                 $from = $fromAddresses[0]->geometry->location->lat . "," . $fromAddresses[0]->geometry->location->lng;
@@ -371,6 +384,97 @@ class SmsController extends Controller
         $sms = Yii::t('sms', 'You will not receive daily SMS');
 
         SmsSender::queueSend($msisdn, $sms);
+
+        return $status;
+    }
+
+    public function actionReport($msisdn, $paramString)
+    {
+        $status = Controller::EXIT_CODE_NORMAL;
+
+        $this->loadSettings($msisdn);
+
+        // route command and three parameters
+        if (preg_match('/(.*)( at )(.*)/i', $paramString, $commandParams)) {
+            // remove the all matching
+            array_shift($commandParams);
+
+            // three parameters to route command
+            if (count($commandParams) == 3) {
+                $incidentType = $commandParams[0];
+                $location = $commandParams[2];
+
+                $incident = new Incident();
+
+                $incidentText = 'unknown event';
+
+                switch ($incidentType) {
+                    case 'construction':
+                        $incident->type = 1;
+                        $incidentText = 'construction';
+                        break;
+                    case 'event':
+                        $incident->type = 2;
+                        $incidentText = 'event';
+                        break;
+                    case 'congestion':
+                        $incident->type = 3;
+                        $incidentText = 'congestion';
+                        break;
+                    case 'accident':
+                        $incident->type = 4;
+                        $incidentText = 'accident';
+                        break;
+                    default:
+                        $incident->type = 0;
+                        $incidentText = 'unknown event';
+                }
+
+                $incidentLocation = Yii::$app->googleMaps->geocode($location);
+
+                if (count($incidentLocation) > 0) {
+
+                    $incident->lat = $incidentLocation[0]->geometry->location->lat;
+                    $incident->lng = $incidentLocation[0]->geometry->location->lng;
+                    $incident->location = $incidentLocation[0]->formatted_address;
+                    $incident->description = ucfirst($incidentText);
+                    $incident->severity = 1;
+                    $incident->eventCode = 1;
+                    $incident->startTime = time();
+                    $incident->endTime = $incident->startTime + (30 * 60);  // 30 minutes
+                    $incident->delayFromFreeFlow = 10;
+                    $incident->delayFromTypical = 10;
+                    $incident->created_at = 0;
+                    $incident->updated_at = 0;
+
+                    //disable it, till it is confirmed by the admin
+                    $incident->enabled = 0;
+
+                    $incident->save();
+
+                    $sms = Yii::t('sms', 'Thank you for reporting {incident} at {location}', [
+                        'incident' => Yii::t('sms', $incidentText),
+                        'location' => Yii::t('sms', $incidentLocation[0]->formatted_address)
+                    ]);
+
+                    SmsSender::queueSend($msisdn, $sms);
+                }
+                else{
+                    $sms = Yii::t('Sorry, {location} is not a valid location', [
+                        'location' => $location,
+                    ]);
+
+                    SmsSender::queueSend($msisdn, $sms);
+
+                }
+            }
+            else{
+                $status = Controller::EXIT_CODE_ERROR;
+            }
+        }
+        else{
+            $status = Controller::EXIT_CODE_ERROR;
+        }
 
         return $status;
     }
--- app/environments/dev/common/config/params-local.php
+++ app/environments/dev/common/config/params-local.php
@@ -3,4 +3,5 @@ return [
 //    'defaultLocation' => '33.729388,73.093146', // Islamabad,Pakistan (http://www.latlong.net/convert-address-to-lat-long.html)
     'defaultLocation' => 'Pakistan',
     'smsKeyword' => 'TUP',
+    'smsShortCode' => '8655',
 ];
